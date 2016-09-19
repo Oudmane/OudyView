429b58a8783c026fa28a79323b8a4e1755171c6b
Ayoub Oudmane <ayoub@oudmane.me>
2016-09-19 23:51:27 +0100

    Rewrite with better Events
    OudyAPI 1.1

--- OudyView.js
+++ OudyView.js
@@ -3,7 +3,8 @@ var OudyAPI = require('oudyapi'),
     formSerializer = require('form-serializer'),
     UIkit = require('uikit-rtl'),
     OudyView,
-    OudyJS;
+    OudyJS,
+    events = jQuery({});
 
 module.exports = {
     state: null,
@@ -41,14 +42,14 @@ module.exports = {
         };
         OudyView.modal.on({
             'show.uk.modal': function(){
-                OudyView.events.show();
+                OudyView.trigger('show');
             },
             'hide.uk.modal': function(){
                 if(!OudyView.forced)
-                    OudyView.events.hide();
+                    OudyView.trigger('hide');
                 if(OudyView.reloadOnClose) {
                     OudyView.reloadOnClose = false;
-                    OudyView.events.reloadOnClose();
+                    OudyView.trigger('reload');
                 }
                 OudyView.forced = false;
             }
@@ -104,41 +105,39 @@ module.exports = {
                 return false;
             });
         }
-        OudyAPI.callbacks['oudyview'] = this.render;
+        OudyAPI.on('before:oudyview', function(event, request) {
+            OudyView.trigger('before', request);
+            jQuery(OudyView.modal.dialog).html('<a class="uk-modal-close uk-close"></a><div><div class="uk-margin"><div class="uk-modal-spinner"></div></div></div>');
+            OudyView.modal.show();
+        });
+        OudyAPI.on('success:oudyview', function(event, response) {
+            OudyView.trigger('success', response);
+            OudyView.render(response.response);
+        });
+        OudyAPI.on('complete:oudyview', function(event, response) {
+            OudyView.trigger('complete', response);
+        });
     },
     request: function(request) {
         this.state = jQuery.extend({}, request);
-        request.beforeSend = function(request) {
-            OudyView.events.beforeSend(request);
-            jQuery(OudyView.modal.dialog).html('<a class="uk-modal-close uk-close"></a><div><div class="uk-margin"><div class="uk-modal-spinner"></div></div></div>');
-            OudyView.modal.show();
-        };
         request.interface = 'oudyview';
         OudyAPI.send(request);
     },
     render: function(view) {
         jQuery(OudyView.modal.dialog).html(view);
-        OudyView.events.render(view);
         OudyView.modal.resize();
         if((form = OudyView.modal.dialog.find('form[oudyview-confirm]')).length)
             form.data('serialize', form.serialize());
         if(OudyView.modal.dialog.find('[oudyview-confirm="reload"]').length)
             OudyView.reloadOnClose = true;
-    },
-    events: {
-        open: function(event) {},
-        close: function(event) {},
-        message: function(event) {},
-        error: function(event) {},
-        beforeSend: function(request) {},
-        render: function(view) {},
-        show: function(view) {},
-        hide: function(view) {},
-        reloadOnClose: function() {}
+        OudyView.trigger('render', view);
     },
     strings: {
         confirm: {
             message: '<h1>You\'ll lose  all changes</h1><p>Are you sure you want to close ?</p>'
         }
-    }
+    },
+    on: events.on.bind(events),
+    one: events.one.bind(events),
+    trigger: events.trigger.bind(events)
 };
